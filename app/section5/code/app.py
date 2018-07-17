4d14224d822034dcab6e273d446fff83c8e04466
primusdj <primusdj@asu.edu>
2018-07-16 22:23:15 -0700

    additional changes to the section5 files, probably will not work

--- app/section5/code/app.py
+++ app/section5/code/app.py
@@ -1,8 +1,10 @@
 from flask_restful import Api
-from flask import Flask
+from flask import Flask, jsonify
 from flask_jwt import JWT
 
-from security import authenticate, identity
+from datetime import timedelta
+
+from security import authenticate, identity as identity_function
 from user import UserRegister
 from item import Item, Items
 
@@ -15,10 +17,23 @@ api = Api(app)
 app.secret_key = 'donotmakepublic'
 
 # the json web token, JWT, help(JWT)
+# tut_flask_api/docs/flask-jwt-guide.pdf
+# configuration of the jwt
+app.config['JWT_AUTH_URL_RULE'] = '/login'
+app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
+app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
+
 # __init__(self, app=None, authentication_handler=None, identity_handler=None)
+# jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity_function) # just an alias
+@jwt.auth_response_handler
+def customized_responsde_handler(access_token, identity):
+	# import jsonify module from flask 
+	return jsonify({'access_token':access_token.decode('utf-8'),'user_id':identity.id})
 
-jwt = JWT(app, authenticate, identity)
-
+@jwt.error_handler
+def customized_error_handler(error):
+	return jsonify({'message': error.description,'code': error.status_code,}), error.status_code
 # storage (in memory database)
 # items = [] # replaced with a sqlite3 database
 
@@ -28,4 +43,13 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(Items, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(host='10.0.2.15', port='58080', debug=True)
+
+
+# interesting point.  This is our apps entry point, BUT if we want to
+# 	import this app.py file for whatever reason, with out the if statement
+#   the flask app on import will run the app.run() method and start the
+#	flask service (not what you want on an import)
+# python behavior the entry point is the __main__, so if users.py is run
+#	via the command line it gets the meta name __main__
+if __name__ == '__main__':
+	app.run(host='10.0.2.15', port='58080', debug=True)
--- app/section5/code/item.py
+++ app/section5/code/item.py
@@ -3,6 +3,18 @@ from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 
 class Item(Resource):
+	item_parser = reqparse.RequestParser()
+	# item_parser.add_argument(
+	# 	'name',
+	# 	type = str,
+	# 	required = True,
+	# 	help = "Null value, this is a required field.")
+	item_parser.add_argument(
+		'price',
+		type = float,
+		required = True,
+		help = "Null value, this is a required field." )
+
 	def find_item(self, name):
 		"""
 			This method is used to find an item by name in the database
@@ -27,15 +39,15 @@ class Item(Resource):
 
 		# return next(filter(lambda item: item['name'] == name, items), None)
 
-	def validate_data_request(self):
-		parser = reqparse.RequestParser()
-		parser.add_argument(
-			'price',
-			type = float,
-			required = True,
-			help = "Null value, this field is required." )
+	def insert_item(self, name, price):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+		sql_insert_item = "INSERT INTO items (name,price) VALUES (?, ?)"
 
-		return parser.parse_args()
+		cursor.execute(sql_insert_item, (name, price,))
+		connection.commit()
+		connection.close()
+
 
 	@jwt_required()
 	def get(self, name):
@@ -46,46 +58,82 @@ class Item(Resource):
 	
 	@jwt_required()
 	def post(self, name):
-		item_check = self.find_item(name)
-		if item_check:
-			return {'message':'Found existing item {}.'.format(item_check['name'])}, 400
 		
-		data = self.validate_data_request()
+		data = self.item_parser.parse_args()
 		# data = request.get_json() 
 			# ways to deal with request payloads (content-type = application/json)
 			# force=True  # deal with the junk data
 			# silent=True # return null but not an error
 
-		item = {'name': name, 'price': data['price']}
-		items.append(item)
-
-		return item, 201
+		item_check = self.find_item(name)
+		if item_check:
+			return {'message':'Found existing item {}.'.format(item_check['name'])}, 400
+		
+		try:
+			self.insert_item(name, data['price'])
+		except:
+			return {'message':'Error, failed to add item {}'.format(name)}, 500
+		
+		return {'message':'Item {} added.'.format(name)}, 201
 	
 	@jwt_required()
 	def delete(self, name):
-		item_check = self.find_item(name)
-		if item_check is None:
-			return {'message':'Item {} does not exist.'.format(name)}, 400
+		# item_check = self.find_item(name)
+		# if item_check is None:
+		# 	return {'message':'Item {} does not exist.'.format(name)}, 400
 
-		global items
-		items = list(filter(lambda item: item['name'] != name, items))
+		# global items
+		# items = list(filter(lambda item: item['name'] != name, items))
+		# return {'message':'Item {} has been deleted.'.format(name)}, 200
+
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		sql_delete_item = "DELETE FROM items WHERE name = ?"
+
+		cursor.execute(sql_delete_item, (name,))
+		connection.commit()
+		connection.close()
 		return {'message':'Item {} has been deleted.'.format(name)}, 200
 
 	@jwt_required()
 	def put(self, name):
-		data = self.validate_data_request()
+		data = self.item_parser.parse_args()
 
 		item = self.find_item(name)
 		if item:
-			item.update(data)
+			try:
+				self.update_item(item)
+			except:
+				return {'message':'Error, failed to update item {}'.format(item['name'])}, 500
+			return {'message':'Item {} updated to price {}.'.format(item['name'],item['price'])}, 201
 		else:
-			item = {'name': name, 'price': data['price']}
-			items.append(item)
-		return item, 200
+			try:
+				self.insert_item(name, data['price'])
+			except:
+				return {'message':'Error, failed to add item {}'.format(name)}, 500
+			return {'message':'Item {} added.'.format(name)}, 201
+
+	def update_item(self, item):
+		sql_update_item = "UPDATE items SET price = ? WHERE name = ?"
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+		cursor.execute(sql_update_item, (item['price'],item['name'],))
+		connection.commit()
+		connection.close()
 
 
 class Items(Resource):
 	@jwt_required()
 	def get(self):
-		return {'items': items}, 202
+		sql_items = "SELECT name, price FROM items"
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+		rows = cursor.execute(sql_items)
+		
+		# items_list = [ {row['name']: row['price']} for row in rows ]
+		items_list = [ {'name':row[0],'price':row[1]} for row in rows ]
+		connection.close()
+		return {'items': items_list}, 202
+		
 
